47d32c660a6924fc84ed4576a3f1da37c3864034
Vedant Barve <barve.vedant@outlook.com>
2022-11-12 10:41:27 +0530

    clear firebase storage

--- functions/index.js
+++ functions/index.js
@@ -3,13 +3,15 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-exports.scheduleDelete = functions.pubsub.topic('scheduleDelete').onPublish(async message =>{
+exports.scheduleDelete = functions.region("asia-south1").pubsub.topic('scheduleDelete').onPublish(async message =>{
     const rooms = await admin.firestore().collection("rooms").listDocuments();
     rooms.forEach(async data=>{
+        
         await admin.firestore().collection("rooms").doc(data.id).delete();
     },);
     const files = await admin.firestore().collection("files").listDocuments();
     files.forEach(async data=>{
         await admin.firestore().collection("files").doc(data.id).delete();
     },);
+    await admin.storage().bucket().deleteFiles();
 },);
